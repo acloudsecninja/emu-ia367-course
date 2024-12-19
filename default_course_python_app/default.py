2a16e9b6024c6b8f09a83e7051f7d3a4cfbcd0f5
acloudsecninja <rweber12@emich.edu>
2024-12-19 06:14:23 -0500

    upload lab1 code and configuration

--- default_course_python_app/default.py
+++ default_course_python_app/default.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Welcome to My Flask App!</h1><p>This is a simple Flask application.</p>"
+    return "<h1>Welcome to My Flask App!</h1><p>This is a simple Flask application and only using flask.</p>"
 
 if __name__ == "__main__":
     app.run(debug=True)
